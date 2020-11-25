4b582a8c8ffa516303d44f07ed06424cc3c42680
Ken Holston <git.kholston@gmail.com>
2020-11-25 11:52:50 -0500

    setup initial routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const userController = require("../controllers/userController");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get("/sign-up", userController.sign_up_get);
+router.post('/sign-up', userController.sign_up_post);
+router.get('/sign-in', userController.sign_in_get);
+router.post('/sign-in', userController.sign_in_post);
+router.get('/log-out', userController.log_out_get);
 
 module.exports = router;
