b0e0e068ffc64f6ac574afd00863547e5ac43eca
Ken Holston <git.kholston@gmail.com>
2020-11-25 11:53:09 -0500

    update user model

--- models/user.js
+++ models/user.js
@@ -1,32 +1,55 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require("bcryptjs");
+const SALT_WORK_FACTOR = 10;
 
-
-UserSchema = new Schema(
-  {
-    first_name: { type: String, required: true, maxlength: 100 },
-    last_name: { type: String, required: true, maxlength: 100 },
-    username: { type: String, required: true, maxlength: 13 },
-    password: { type: String, required: true, minlength: 6 },
-    member_status: { type: String, 
-        enum:['User','Member','Admin'], 
-        default:'User', 
-        required:true 
-    }
-  },
-  {timestamps:true}
-);
+const UserSchema = new Schema({
+  first_name: { type: String, required: true, maxlength: 100 },
+  last_name: { type: String, required: true, maxlength: 100 },
+  username: { type: String, required: true, maxlength: 13 },
+  password: { type: String, required: true, minlength: 6 },
+  member_status: {
+    type: String,
+    enum: ['User', 'Member', 'Admin'],
+    default: 'User',
+    required: true
+  }
+}, { timestamps: true });
 
 UserSchema
   .virtual("name")
-  .get(()=>{
+  .get(() => {
     return this.first_name + " " + this.last_name;
-  })
+  });
 
 UserSchema
   .virtual('url')
-  .get(()=>{
+  .get(() => {
     return "/users/user/" + this._id;
   })
 
-module.exports = mongoose.model("User",  UserSchema);
+UserSchema.pre('save', function(next) {
+  let user = this;
+  if (!user.isModified('password')) {
+    return next();
+  }
+
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if (err) { return next(err) }
+
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) { return next(err) }
+      user.password = hash;
+      next();
+    })
+  })
+})
+
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  })
+}
+
+module.exports = mongoose.model("User", UserSchema);
