b2cd807646905391d26f527ac98b7af8d5c6a588
Ken <kennyk3n@live.com>
2020-12-18 18:00:03 -0500

    format code with eslint

--- routes/messages.js
+++ routes/messages.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
 const message_controller = require('../controllers/messageController');
 
--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,22 @@
 var express = require('express');
 var router = express.Router();
-const userController = require("../controllers/userController");
+const userController = require('../controllers/userController');
 
-router.get("/sign-up", userController.sign_up_get);
+router.get('/sign-up', userController.sign_up_get);
 router.post('/sign-up', userController.sign_up_post);
 router.get('/sign-in', userController.sign_in_get);
 router.post('/sign-in', userController.sign_in_post);
 router.get('/log-out', userController.log_out_get);
 
-router.get("/profile", userController.user_profile_get);
-router.get('/membership-upgrade', userController.upgrade_membership_get);
-router.post('/membership-upgrade', userController.upgrade_membership_post);
+router.get('/profile', userController.user_profile_get);
+router.get(
+  '/membership-upgrade',
+  userController.upgrade_membership_get,
+);
+router.post(
+  '/membership-upgrade',
+  userController.upgrade_membership_post,
+);
 
 router.get('/admin', userController.admin_form_get);
 router.post('/admin', userController.admin_form_post);
